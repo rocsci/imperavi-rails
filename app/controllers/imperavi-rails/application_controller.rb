f570fb2474f4aaa3be44ce856cf6d99f39409737
tanraya <demerest@gmail.com>
2012-02-02 13:54:50 +0600

    Removed unnecessary files

--- app/controllers/imperavi-rails/application_controller.rb
+++
@@ -1,4 +0,0 @@
-module ImperaviRails
-  class ApplicationController < ActionController::Base
-  end
-end
--- app/helpers/imperavi-rails/application_helper.rb
+++
@@ -1,7 +0,0 @@
-module ImperaviRails
-  module ApplicationHelper
-  	def require_imperavi
-      stylesheet_link_tag 'imperavi/redactor'
-  	end
-  end
-end
