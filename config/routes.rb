a6c320a7424ac032710ddcd6307a4e6e286aa44c
tanraya <demerest@gmail.com>
2012-02-02 12:03:27 +0600

    Added routes for imperavi controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 ImperaviRails::Engine.routes.draw do
+  get '/file'       => 'imperavi#file',       :as => :file
+  get '/file_edit'  => 'imperavi#file_edit',  :as => :file_edit
+  get '/image'      => 'imperavi#image',      :as => :image
+  get '/image_edit' => 'imperavi#image_edit', :as => :image_edit
+  get '/link'       => 'imperavi#link',       :as => :link
+  get '/table'      => 'imperavi#table',      :as => :table
+  get '/video'      => 'imperavi#video',      :as => :video
+  get '/toolbar'    => 'imperavi#toolbar',    :as => :toolbar
+  get '/lang'       => 'imperavi#lang',       :as => :lang
 end
