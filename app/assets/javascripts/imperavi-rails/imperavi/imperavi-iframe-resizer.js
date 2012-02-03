cad19122747561af78b9fd67015fc99ec1968d32
tanraya <demerest@gmail.com>
2012-02-04 02:19:51 +0600

    Added iframe resizer

--- app/assets/javascripts/imperavi-rails/imperavi/imperavi-iframe-resizer.js
+++ app/assets/javascripts/imperavi-rails/imperavi/imperavi-iframe-resizer.js
@@ -3,6 +3,7 @@
   
   $.fn.ImperaviIframeResizer.prototype = {
     iframe : null,
+    el     : null,
 
     initialize: function(iframe, o) {
       this.iframe = iframe
@@ -12,7 +13,11 @@
     },
 
     build: function() {
-      
+      this.el = $(document.createElement('div'))
+        .addClass('imperavi-resizer')
+        .insertAfter(this.iframe.el)
+
+      this.el.append($(document.createElement('div')))
     }
   }
 })(jQuery);
