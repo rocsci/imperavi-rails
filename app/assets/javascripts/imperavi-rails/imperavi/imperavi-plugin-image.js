094063db25d57aa6a75a2237cc3b0fbddc574dad
tanraya <demerest@gmail.com>
2012-02-09 13:11:07 +0600

    Styling image dialog

--- app/assets/javascripts/imperavi-rails/imperavi/imperavi-plugin-image.js
+++ app/assets/javascripts/imperavi-rails/imperavi/imperavi-plugin-image.js
@@ -9,8 +9,11 @@
         title  : 'Insert image',
         width  : 800,
         height : 600,
+        onRemove : function() {
+          alert('fuck yeah!')
+          this.hide()
+        },
         onOkay : function() {
-          //alert(this.el.find('textarea').val())
           this.hide()
         }
       })
@@ -21,19 +24,22 @@
     },
 
     build: function() {
-      this.tabs_area = this.build_tabs_area()
+      // Add 'remove' button
+      this.removeButton = this.dialog.addButton('Remove', 'remove')
+      this.removeButton.hide();
 
-      this.pick_tab = this.build_tab('Pick image', '')
-        .appendTo(this.tabs_area)
+      // Create tabs and content for tabs
+      this.tabsArea = this.build_tabs_area()
+
+      this.pickTab = this.build_tab('Pick image', this.buildPickTabContent())
+        .appendTo(this.tabsArea)
         .addClass('current')
 
-      this.upload_tab = this.build_tab('Upload image', '')
-        .appendTo(this.tabs_area)
+      this.uploadTab = this.build_tab('Upload image', this.buildUploadTabContent())
+        .appendTo(this.tabsArea)
 
-      this.link_tab = this.build_tab('Link to image', '')
-        .appendTo(this.tabs_area)
-      
-      this.dialog.setContent(this.tabs_area)
+      this.dialog.setContent(this.tabsArea)
+      this.switchTabs();
     },
 
     build_tabs_area: function() {
@@ -47,6 +53,7 @@
       var section = $(document.createElement('section'))
       var heading = $(document.createElement('h3'))
         .html(title)
+        .addClass('imperavi-tab')
         .appendTo(section)
 
       var content = $(document.createElement('div'))
@@ -54,6 +61,63 @@
         .html(content)
 
       return section
+    },
+
+    switchTabs: function() {
+      var currentTab = this.pickTab
+      
+      this.tabsArea.on('click', '.imperavi-tab', function(e){
+        e.preventDefault();
+        
+        if (currentTab.length) {
+          currentTab.removeClass('current')
+        }
+
+        currentTab = $(this).closest('section').addClass('current')
+      })
+    },
+
+    buildPickTabContent: function() {
+      
+    },
+
+    buildUploadTabContent: function() {
+      // Create wrapper
+      var wrapper = $(document.createElement('div'))
+        .attr('id', 'upload-wrapper')
+
+      // Create place for thumbnail
+      var thumbnail = $(document.createElement('figure'))
+        .addClass('thumbnail')
+        .appendTo(wrapper)
+
+      // Choose image from disk
+      var imageLocal = this.build_input('Choose image from disk', 'image', 'file')
+        .appendTo(wrapper)
+
+      // Image url
+      var imageUrl = this.build_input('or specify link to image', 'image-url', 'text')
+        .appendTo(wrapper)
+
+      return wrapper
+    },
+
+    build_input: function(title, name, type) {
+      var wrapper = $(document.createElement('div'))
+        .addClass('field')
+
+      var label = $(document.createElement('label'))
+        .attr('for', name)
+        .html(title)
+        .appendTo(wrapper)
+
+      var url = $(document.createElement('input'))
+        .attr('type', type)
+        .attr('name', name)
+        .attr('id', name)
+        .appendTo(wrapper)
+
+      return wrapper
     }
   }
 })(jQuery);
